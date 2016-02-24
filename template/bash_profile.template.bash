cb239fdb283e18e5cadb0a20dd84577296bcc102
Chehab Mustafa-Hilmy <m@chehab.me>
2016-02-24 14:45:38 +0200

    update powerline-ml theme and set as default

--- template/bash_profile.template.bash
+++ template/bash_profile.template.bash
@@ -5,7 +5,7 @@ export BASH_IT="{{BASH_IT}}"
 
 # Lock and Load a custom theme file
 # location /.bash_it/themes/
-export BASH_IT_THEME='bobby'
+export BASH_IT_THEME='powerline-ml'
 
 # Your place for hosting Git repos. I use this for private repos.
 export GIT_HOSTING='git@git.domain.com'
